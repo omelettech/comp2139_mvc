71e88ef980ddcc3ae28b5aa185f3ee702d3b7f91
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-01-19 09:47:09 -0500

    Complete Lab 2 week 2

---
+++ comp2139_mvc/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace comp2139_mvc.Models
+{
+    public class Project
+    {
+        [Required] //constraints
+        public int ProjId { get; set; }
+        public required string ProjName { get; set; } //default value = "NewProj"
+        public string? Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? CreatedDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DueTime { get; set; }
+
+        public string? Status {  get; set; } //default value = "not started" make it non-nullable later
+
+        public Project()
+        {
+
+        }
+    }
+}
