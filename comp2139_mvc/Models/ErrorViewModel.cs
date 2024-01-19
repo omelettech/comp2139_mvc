66aa92e3ddca6db6105662f1c14deba9c5f3b3b5
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-01-19 08:15:04 -0500

    Add project files.

---
+++ comp2139_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace comp2139_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
